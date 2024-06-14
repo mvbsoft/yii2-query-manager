1a6f30a8cd7d02aed2af3cd82230f4340424eca1
Malinovskyi <vitaliy@lcgroups.com>
2024-06-14 12:36:33 +0300

    Revert prev commit

--- src/operators/ContainsStringOperator.php
+++ src/operators/ContainsStringOperator.php
@@ -49,7 +49,11 @@ class ContainsStringOperator extends OperatorAbstract
         $searchValue = strval($searchValue);
         $value = strval($value);
 
-        return  mb_strpos($value, $searchValue) !== false;
+        // Escape special characters
+        $escapedSearchValue = preg_quote($searchValue, '/');
+
+        // Check if $searchValue is found within $value using the regular expression without considering case sensitivity
+        return preg_match("/{$escapedSearchValue}/i", $value) === 1;
     }
 
     public static function mongodbConditions(string $column, $searchValue) : array
